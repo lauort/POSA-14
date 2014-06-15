ee9043b25a1a75cbb22500f4175d8c37756d7117
Laureano <laureano.ortega@oramon.nl>
2014-06-15 22:32:13 +0200

    update all project

--- assignments/week-3-assignment-2/src/edu/vuum/mocca/SimpleAtomicLong.java
+++ assignments/week-3-assignment-2/src/edu/vuum/mocca/SimpleAtomicLong.java
@@ -1,5 +1,3 @@
-// Import the necessary Java synchronization and scheduling classes.
-
 package edu.vuum.mocca;
 
 import java.util.concurrent.locks.ReentrantReadWriteLock;
@@ -9,7 +7,7 @@ import java.util.concurrent.locks.Lock;
  * @class SimpleAtomicLong
  *
  * @brief This class implements a subset of the
- *        java.util.concurrent.atomic.SimpleAtomicLong class using a
+ *        java.util.concurrent.atomic.AtomicLong class using a
  *        ReentrantReadWriteLock to illustrate how they work.
  */
 class SimpleAtomicLong
@@ -18,27 +16,40 @@ class SimpleAtomicLong
      * The value that's manipulated atomically via the methods.
      */
     private long mValue;
-
-
+    
     /**
      * The ReentrantReadWriteLock used to serialize access to mValue.
      */
-    // TODO - add the implementation
 
+    // TODO -- you fill in here by replacing the null with an
+    // initialization of ReentrantReadWriteLock.
+    //private final ReentrantReadWriteLock mRWLock = null;
+	private final ReentrantReadWriteLock mrWLock = new ReentrantReadWriteLock();
+	
     /**
      * Creates a new SimpleAtomicLong with the given initial value.
      */
-    public SimpleAtomicLong(long initialValue) {
-        // TODO - you fill in here
+    public SimpleAtomicLong(long initialValue)
+    {
+        // TODO -- you fill in here
+		mValue = initialValue;
     }
 
     /**
-     * @brief Gets the current value
+     * @brief Gets the current value.
      * 
      * @returns The current value
      */
-    public long get() {
-        // TODO - you fill in here
+    public long get()
+    {
+        long value;
+
+        // TODO -- you fill in here
+		mrWLock.readLock().lock();
+		value = this.mValue;
+		mrWLock.readLock().unlock();
+		
+        return value;
     }
 
     /**
@@ -46,8 +57,17 @@ class SimpleAtomicLong
      *
      * @returns the updated value
      */
-    public long decrementAndGet() {
-        // TODO - you fill in here
+    public long decrementAndGet()
+    {
+        long value = 0;
+
+        // TODO -- you fill in here
+		mrWLock.writeLock().lock();
+		this.mValue--;
+		value=this.mValue;
+		mrWLock.writeLock().unlock();
+
+        return value;
     }
 
     /**
@@ -55,8 +75,16 @@ class SimpleAtomicLong
      *
      * @returns the previous value
      */
-    public long getAndIncrement() {
-        // TODO - you fill in here
+    public long getAndIncrement()
+    {
+        long value = 0;
+
+        // TODO -- you fill in here
+        mrWLock.writeLock().lock();
+		value=this.mValue;
+		this.mValue++;
+		mrWLock.writeLock().unlock();
+        return value;
     }
 
     /**
@@ -64,8 +92,18 @@ class SimpleAtomicLong
      *
      * @returns the previous value
      */
-    public long getAndDecrement() {
-        // TODO - you fill in here
+    public long getAndDecrement()
+    {
+        long value = 0;
+
+        // TODO -- you fill in here
+        mrWLock.writeLock().lock();
+		value=this.mValue;
+		this.mValue--;
+		mrWLock.writeLock().unlock();
+		
+
+        return value;
     }
 
     /**
@@ -73,8 +111,16 @@ class SimpleAtomicLong
      *
      * @returns the updated value
      */
-    public long incrementAndGet() {
-        // TODO - you fill in here
+    public long incrementAndGet()
+    {
+        long value = 0;
+
+        // TODO -- you fill in here
+        mrWLock.writeLock().lock();
+		this.mValue++;
+        value = this.mValue; 
+        mrWLock.writeLock().unlock();
+        return value;
     }
 }
 
--- assignments/week-3-assignment-2/src/edu/vuum/mocca/SimpleSemaphore.java
+++ assignments/week-3-assignment-2/src/edu/vuum/mocca/SimpleSemaphore.java
@@ -15,25 +15,34 @@ import java.util.concurrent.locks.Condition;
  */
 public class SimpleSemaphore {
     /**
-     * Define a Lock to protect the critical section.
+     * Define a ReentrantLock to protect the critical section.
      */
     // TODO - you fill in here
+	private ReentrantLock lock = new ReentrantLock();
 
     /**
      * Define a Condition that waits while the number of permits is 0.
      */
     // TODO - you fill in here
-
-    /**
+	Condition condition;
+	
+	/**
      * Define a count of the number of available permits.
      */
     // TODO - you fill in here.  Make sure that this data member will
     // ensure its values aren't cached by multiple Threads..
+	Integer count;
 
     public SimpleSemaphore(int permits, boolean fair) {
         // TODO - you fill in here to initialize the SimpleSemaphore,
         // making sure to allow both fair and non-fair Semaphore
         // semantics.
+    	this.count = permits;
+    	if(fair) {
+    		lock = new ReentrantLock(fair);
+    	}
+    	condition = lock.newCondition();
+    	
     }
 
     /**
@@ -42,6 +51,15 @@ public class SimpleSemaphore {
      */
     public void acquire() throws InterruptedException {
         // TODO - you fill in here.
+    	lock.lock();
+    	try {
+    		while(count == 0)
+    			condition.await();
+    		count--;
+    	} finally {
+    		lock.unlock();
+    	}
+    	    	
     }
 
     /**
@@ -50,13 +68,31 @@ public class SimpleSemaphore {
      */
     public void acquireUninterruptibly() {
         // TODO - you fill in here.
+    	lock.lock();
+    	try {
+    		while(count == 0)
+    			condition.awaitUninterruptibly();
+    		count--;
+    		
+    	} finally {
+    		lock.unlock();
+    	}
     }
 
     /**
      * Return one permit to the semaphore.
      */
-    public void release() {
+    void release() {
         // TODO - you fill in here.
+    	 lock.lock();
+         try {
+        	count++;
+			condition.signal();
+			
+         } finally {
+           lock.unlock();
+         }
+    	
     }
 
     /**
@@ -65,6 +101,6 @@ public class SimpleSemaphore {
     public int availablePermits() {
         // TODO - you fill in here by changing null to the appropriate
         // return value.
-        return null;
+        return count;
     }
 }
