50f791cbca7a3306d85ad107d0b0acb2288ca9ea
Laureano <laureano.ortega@oramon.nl>
2014-06-30 21:25:23 +0200

    Update with assignament 6

--- assignments/week-6-assignment-5/W6-A5-ThreadedDownloads-StartedServices/src/edu/vuum/mocca/DownloadActivity.java
+++ assignments/week-6-assignment-5/W6-A5-ThreadedDownloads-StartedServices/src/edu/vuum/mocca/DownloadActivity.java
@@ -2,6 +2,7 @@ package edu.vuum.mocca;
 
 import java.lang.ref.WeakReference;
 
+import android.content.Intent;
 import android.os.Handler;
 import android.os.Message;
 import android.view.View;
@@ -78,6 +79,8 @@ public class DownloadActivity extends DownloadBase {
                 // bitmap that's been downloaded and returned to
                 // the DownloadActivity as a pathname who's Bundle
             	// key is defined by DownloadUtils.PATHNAME_KEY
+            	String filepath = msg.getData().getString(DownloadUtils.PATHNAME_KEY);
+                activity.displayBitmap(filepath);
             }
     	}
     }
@@ -101,13 +104,16 @@ public class DownloadActivity extends DownloadBase {
      */
     public void runService(View view) {
     	String which = "";
-
+    	
+    	Intent intent;
     	switch (view.getId()) {
         case R.id.intent_service_button:
             // TODO - You fill in here to start the
             // DownloadIntentService with the appropriate Intent
             // returned from the makeIntent() factory method.
 
+            intent = DownloadIntentService.makeIntent(this, handler , this.getUrlString());
+            startService(intent);
             which = "Starting DownloadIntentService";
             break;
         
@@ -115,7 +121,8 @@ public class DownloadActivity extends DownloadBase {
             // TODO - You fill in here to start the
             // ThreadPoolDownloadService with the appropriate Intent
             // returned from the makeIntent() factory method.
-
+            intent = ThreadPoolDownloadService.makeIntent(this, handler, this.getUrlString());
+            startService(intent);
             which = "Starting ThreadPoolDownloadService";
             break;
         
